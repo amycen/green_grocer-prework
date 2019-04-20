e919cd4a4eb5cd70cb67400698193a10eae17a62
Amy Cen <amy.cen@blackrock.com>
2019-04-20 04:32:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,12 +2,13 @@ require "pry"
 def consolidate_cart(cart)
   # code here
   whole_cart = cart.flatten
+      binding.pry
   cart.each do |item_hash|
     item_hash.each do |item, attrib|
       whole_cart[item] ||= {}
       whole_cart[item][:count] ||= 0
       whole_cart[item][:count] += 1
-      binding.pry
+  
     end
   end
   whole_cart
